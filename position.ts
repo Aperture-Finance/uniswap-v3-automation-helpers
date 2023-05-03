99ecc79d686a499e792cef188f65fabc88bf9003
Peiqian Li <peiqian@aperture.finance>
2023-05-03 15:18:58 -0700

    Add getCollectableTokenAmounts().

--- position.ts
+++ position.ts
@@ -1,12 +1,11 @@
-import { BigintIsh, Token } from '@uniswap/sdk-core';
+import { BigintIsh, CurrencyAmount, Token } from '@uniswap/sdk-core';
 import { Position } from '@uniswap/v3-sdk';
 import { Provider } from '@ethersproject/abstract-provider';
 import { getChainInfo } from './chain';
-import {
-  ERC20__factory,
-  INonfungiblePositionManager__factory,
-} from '@aperture_finance/uniswap-v3-automation-sdk/typechain-types';
+import { INonfungiblePositionManager__factory } from '@aperture_finance/uniswap-v3-automation-sdk/typechain-types';
 import { getPoolFromBasicPositionInfo } from './pool';
+import { getToken } from './currency';
+import { BigNumber } from 'ethers';
 
 export interface BasicPositionInfo {
   token0: Token;
@@ -29,16 +28,10 @@ export async function getBasicPositionInfo(
       provider,
     );
   const positionInfo = await nonfungiblePositionManager.positions(positionId);
-  const token0Address = positionInfo.token0;
-  const token1Address = positionInfo.token1;
-  const token0Contract = ERC20__factory.connect(token0Address, provider);
-  const token1Contract = ERC20__factory.connect(token1Address, provider);
-  const [token0Decimals, token1Decimals] = await Promise.all([
-    token0Contract.decimals(),
-    token1Contract.decimals(),
+  const [token0, token1] = await Promise.all([
+    getToken(positionInfo.token0, chainId, provider),
+    getToken(positionInfo.token1, chainId, provider),
   ]);
-  const token0 = new Token(chainId, token0Address, token0Decimals);
-  const token1 = new Token(chainId, token1Address, token1Decimals);
   return {
     token0,
     token1,
@@ -75,4 +68,59 @@ export async function getUniswapSDKPosition(
     chainId,
     provider,
   );
+}
+
+/**
+ * Finds the amount of collectable tokens in the position.
+ * The collectable amount is most likely accrued fees accumulated in the position, but can be from a prior decreaseLiquidity() call which has not been collected.
+ * @param chainId Chain id.
+ * @param positionId Position id.
+ * @param provider Ethers provider.
+ * @param basicPositionInfo Basic position info, optional; if undefined, one will be constructed.
+ * @returns A promise that resolves to collectable amount of the two tokens in the position.
+ */
+export async function getCollectableTokenAmounts(
+  chainId: number,
+  positionId: number,
+  provider: Provider,
+  basicPositionInfo?: BasicPositionInfo,
+): Promise<{
+  token0Amount: CurrencyAmount<Token>;
+  token1Amount: CurrencyAmount<Token>;
+}> {
+  if (basicPositionInfo === undefined) {
+    basicPositionInfo = await getBasicPositionInfo(
+      chainId,
+      positionId,
+      provider,
+    );
+  }
+  const chainInfo = getChainInfo(chainId);
+  const npm = INonfungiblePositionManager__factory.connect(
+    chainInfo.uniswap_v3_nonfungible_position_manager,
+    provider,
+  );
+  const owner = await npm.ownerOf(positionId);
+  const MAX_UINT128 = BigNumber.from(2).pow(128).sub(1);
+  const { amount0, amount1 } = await npm.callStatic.collect(
+    {
+      tokenId: positionId,
+      recipient: owner,
+      amount0Max: MAX_UINT128,
+      amount1Max: MAX_UINT128,
+    },
+    {
+      from: owner,
+    },
+  );
+  return {
+    token0Amount: CurrencyAmount.fromRawAmount(
+      basicPositionInfo.token0,
+      amount0.toString(),
+    ),
+    token1Amount: CurrencyAmount.fromRawAmount(
+      basicPositionInfo.token1,
+      amount1.toString(),
+    ),
+  };
 }
