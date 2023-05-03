0c993a00627d7dc87d996255ad5c6b0488134ac4
Peiqian Li <peiqian@aperture.finance>
2023-05-02 21:59:07 -0700

    Helpers for token amount parsing, price parsing, and limit order position minting tx.

---
+++ provider.ts
@@ -0,0 +1,16 @@
+import { ethers } from 'ethers';
+import { providers } from '@0xsequence/multicall';
+import { CHAIN_ID_TO_INFO } from './chain';
+
+/**
+ * Creates a public ethers provider for the specified chain id.
+ * @param chainId chain id must be supported by Aperture's UniV3 Automation platform.
+ * @returns A muticall-wrapped public Infura provider.
+ */
+export function getPublicProvider(chainId: number): providers.MulticallProvider {
+  return new providers.MulticallProvider(
+    new ethers.providers.InfuraProvider(
+      CHAIN_ID_TO_INFO.get(chainId)!.infura_network_id!,
+    ),
+  );
+}
---
+++ tick.ts
@@ -0,0 +1,26 @@
+import { Price, Token } from "@uniswap/sdk-core";
+import { FeeAmount, TICK_SPACINGS, TickMath, nearestUsableTick, priceToClosestTick, tickToPrice } from "@uniswap/v3-sdk";
+
+/**
+ * Finds the closest usable tick for the specified price and pool fee tier.
+ * Price may be specified in either direction, i.e. price of token1 denominated in token0 and price of token0 denominated in token1 both work.
+ * @param price Price of two tokens in the liquidity pool. Either token0 or token1 may be the base token.
+ * @param poolFee Liquidity pool fee tier.
+ * @returns The closest usable tick.
+ */
+export function priceToClosestUsableTick(price: Price<Token, Token>, poolFee: FeeAmount): number {
+    let tick = priceToClosestTick(price);
+    tick = Math.max(tick, TickMath.MIN_TICK);
+    tick = Math.min(tick, TickMath.MAX_TICK);
+    return nearestUsableTick(tick, TICK_SPACINGS[poolFee]);
+}
+
+/**
+ * Aligns price to the closest usable tick.
+ * @param price The price to align.
+ * @param poolFee Liquidity pool fee tier.
+ * @returns The aligned price.
+ */
+export function alignPriceToClosestUsableTick(price: Price<Token, Token>, poolFee: FeeAmount): Price<Token, Token> {
+    return tickToPrice(price.baseCurrency, price.quoteCurrency, priceToClosestUsableTick(price, poolFee));
+}
