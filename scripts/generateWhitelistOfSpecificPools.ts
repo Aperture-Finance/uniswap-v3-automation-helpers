38be3576cafce5152f19a559384988079da42d1c
Peiqian Li <peiqian@aperture.finance>
2023-08-01 16:57:33 -0700

    Update pool whitelist.

--- scripts/generateWhitelistOfSpecificPools.ts
+++ scripts/generateWhitelistOfSpecificPools.ts
@@ -1,44 +1,62 @@
 import { ApertureSupportedChainId } from '@aperture_finance/uniswap-v3-automation-sdk';
-import { Token } from '@uniswap/sdk-core';
 import axios from 'axios';
 import { writeFileSync } from 'fs';
 
 import { getChainInfo } from '../chain';
-import { getTokenPriceListFromCoingecko } from '../price';
+import whitelistedPoolsEthereum from '../data/whitelistedPools-1.json';
+import whitelistedPoolsArbitrum from '../data/whitelistedPools-42161.json';
+import { fetchUniV3PoolsFromGeckoTerminal } from './fetchUniV3PoolsFromGeckoTerminal';
 
 async function generateWhitelistOfSpecificPools(
   chainId: number,
   poolsToFetch: string[],
+  currentWhitelist: any[],
 ) {
-  const response = await axios.post(
-    getChainInfo(chainId).uniswap_subgraph_url!,
-    {
-      operationName: 'fetchSpecificPools',
-      query: `
-        query SpecificPools {
-            pools(where: {id_in: ["${poolsToFetch.join('", "')}"]}) {
-            id
-            totalValueLockedUSD
-            feeTier
-            volumeUSD
-            token0 {
-                id
-                symbol
-                decimals
-                name
-            }
-            token1 {
-                id
-                symbol
-                decimals
-                name
-            }
-            }
-        }`,
-      variables: {},
-    },
-  );
-  const pools = response.data.data.pools;
+  // Add existing whitelisted pools if not on the to-fetch list.
+  const poolsToFetchSet = new Set(poolsToFetch);
+  for (const pool of currentWhitelist) {
+    if (!poolsToFetchSet.has(pool.id.toLowerCase())) {
+      console.log(`Pool ${pool.id} is added to the list to fetch.`);
+      poolsToFetch.push(pool.id.toLowerCase());
+    }
+  }
+
+  console.log(`Fetching information for ${poolsToFetch.length} pools...`);
+  let pools: any[] = [];
+  for (let startIndex = 0; startIndex < poolsToFetch.length; startIndex += 50) {
+    const response = await axios.post(
+      getChainInfo(chainId).uniswap_subgraph_url!,
+      {
+        operationName: 'fetchSpecificPools',
+        query: `
+          query SpecificPools {
+              pools(where: {id_in: ["${poolsToFetch
+                .slice(startIndex, startIndex + 50)
+                .join('", "')}"]}) {
+              id
+              totalValueLockedUSD
+              feeTier
+              volumeUSD
+              token0 {
+                  id
+                  symbol
+                  decimals
+                  name
+              }
+              token1 {
+                  id
+                  symbol
+                  decimals
+                  name
+              }
+              }
+          }`,
+        variables: {},
+      },
+    );
+    pools = pools.concat(response.data.data.pools);
+  }
+
   const USDCe = '0xff970a61a04b1ca14834a43f5de4533ebddb5cc8';
   // Create a set of unique token ids
   const tokens: Set<string> = new Set();
@@ -54,54 +72,84 @@ async function generateWhitelistOfSpecificPools(
     tokens.add(pool.token1.id);
   }
 
-  writeFileSync(
-    `data/whitelistedSpecificPools-${chainId}.json`,
-    JSON.stringify(pools, null, 2),
-    'utf-8',
-  );
+  // Fetch token prices in batches from Coingecko.
+  const tokenArray = Array.from(tokens);
+  let priceList: { [address: string]: number } = {};
+  const { coingecko_asset_platform_id } = getChainInfo(chainId);
+  for (let startIndex = 0; startIndex < tokenArray.length; startIndex += 50) {
+    const priceResponse = await axios.get(
+      `https://api.coingecko.com/api/v3/simple/token_price/${coingecko_asset_platform_id}` +
+        `?contract_addresses=${tokenArray
+          .slice(startIndex, startIndex + 50)
+          .toString()}&vs_currencies=usd`,
+    );
+    priceList = Object.keys(priceResponse.data).reduce(
+      (obj: { [address: string]: number }, address: string) => {
+        obj[address] = priceResponse.data[address]['usd'];
+        return obj;
+      },
+      priceList,
+    );
+  }
 
-  // Convert the Set to an Array of Token objects, then pass to the function
-  const tokenArray = Array.from(tokens).map(
-    (addr) => new Token(chainId, addr, 18),
-  );
-
-  // call coingecko API for all tokens at once
-  const priceList = await getTokenPriceListFromCoingecko(tokenArray);
-
-  // loop over priceList and print information
-  for (const token in priceList) {
+  // Loop over `tokenArray` and print information.
+  for (const token of tokenArray) {
     if (priceList[token]) {
       console.log(`Token ${token}'s price is ${priceList[token]}.`);
     } else {
       console.log(`Token ${token} doesn't have Coingecko price support.`);
     }
   }
+
+  pools
+    .filter((pool) => !priceList[pool.token0.id] || !priceList[pool.token1.id])
+    .forEach((pool) => {
+      console.log(pool);
+    });
+
+  console.log(`Pools before token price filtering: ${pools.length}`);
+  pools = pools.filter(
+    (pool) => priceList[pool.token0.id] && priceList[pool.token1.id],
+  );
+  console.log(`Pools after token price filtering: ${pools.length}`);
+
+  // See if current whitelist is all covered in the new list.
+  const newWhitelistSet = new Set(pools.map((pool) => pool.id.toLowerCase()));
+  for (const pool of currentWhitelist) {
+    if (!newWhitelistSet.has(pool.id.toLowerCase())) {
+      console.log(`Pool ${pool.id} is not in the new whitelist.`);
+    } else {
+      console.log(`Pool ${pool.id} is in the new whitelist.`);
+    }
+  }
+
+  writeFileSync(
+    `data/whitelistedSpecificPools-${chainId}.json`,
+    JSON.stringify(pools, null, 2),
+    'utf-8',
+  );
+
   console.log(
     `Generated ${pools.length} whitelisted pools for chain id ${chainId}, involving ${tokens.size} tokens.`,
   );
 }
 
-generateWhitelistOfSpecificPools(
-  ApertureSupportedChainId.ARBITRUM_MAINNET_CHAIN_ID,
-  [
-    '0x6f38e884725a116c9c7fbf208e79fe8828a2595f', // WETH-USDC 0.01%
-    '0xc473e2aee3441bf9240be85eb122abb059a3b57c', // WETH-USDC 0.3%
-    '0x42fc852a750ba93d5bf772ecdc857e87a86403a9', // WETH-USDC 1%
-    '0xfeaa137f43f88b7f767f5a67978fff8ec11cc6ef', // WETH-FXS (Frax Share) 1%
-    '0x2fe69fc0383ca71f5d76c4c858540cb2be2e10be', // WETH-STG (Stargate) 0.3%
-    '0x04a8cddbb62e3499c8e84ccf77192ed6292bf29d', // gOHM-USDC.e 0.3%
-    '0x25ab7dc4ddcacb6fe75694904db27602175245f1', // LDO-WETH 1%
-    '0x446bf9748b4ea044dd759d9b9311c70491df8f29', // RDNT-WETH 0.3%
-    '0x90d2fb08af9e9323d7cbd364181bda1e7d3c2c2f', // LDO-USDC 1%
-    '0xc94560e81ce1a78b2a5f686a9a913e8560c00234', // SPA-WETH 1%
-  ],
+fetchUniV3PoolsFromGeckoTerminal(
+  'https://app.geckoterminal.com/api/p1/arbitrum/pools?dex=uniswap_v3_arbitrum',
+).then((pools) =>
+  generateWhitelistOfSpecificPools(
+    ApertureSupportedChainId.ARBITRUM_MAINNET_CHAIN_ID,
+    pools.filter((pool) => pool[2] >= 10).map((pool) => pool[0]),
+    whitelistedPoolsArbitrum,
+  ),
 );
-generateWhitelistOfSpecificPools(
-  ApertureSupportedChainId.ETHEREUM_MAINNET_CHAIN_ID,
-  [
-    '0x78235d08b2ae7a3e00184329212a4d7acd2f9985', // LDO-USDC 1%
-    '0x08f68110f1e0ca67c80a24b4bd206675610f445d', // gOHM-USDC 0.3%
-    '0x893f503fac2ee1e5b78665db23f9c94017aae97d', // OHM-USDC 0.3%
-    '0x841820459769cd629b10a36fd12e603938cc2679', // WLD-WETH 1%
-  ],
+
+fetchUniV3PoolsFromGeckoTerminal(
+  'https://app.geckoterminal.com/api/p1/eth/pools?dex=uniswap_v3',
+).then((pools) =>
+  generateWhitelistOfSpecificPools(
+    ApertureSupportedChainId.ETHEREUM_MAINNET_CHAIN_ID,
+    pools.filter((pool) => pool[2] >= 10).map((pool) => pool[0]),
+    whitelistedPoolsEthereum,
+  ),
 );
